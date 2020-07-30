e15ea27e46a49f669c806da4a23bff2592ea5470
TeddyTW <65396509+TeddyTW@users.noreply.github.com>
2020-07-30 16:17:06 +0100

    update

--- Bulk.py
+++ Bulk.py
@@ -8,23 +8,31 @@ import pandas as pd
 from decimal import *
 from scipy.integrate import solve_bvp
 
-def Bulk_Scalar(H, V, r0, LamB=0.0, k: float = 1, m: float = 0.2, visualise: bool = True):
+def Bulk_Scalar(H, V, r0, LamB=0.0, LamV=None, LamH=None, k: float = 1, m: float = 0.2, visualise: bool = True):
     c=np.sqrt(4+(np.power(m,2)/np.power(k,2)))
     E=np.sqrt(4+(np.power(m,2)/np.power(k,2)))-2
     
-    #the EL equation of motion that must be solved (Numerically)    
+    def dU_dx(U, x, r0, k):
+        # Here U is a vector such that y=U[0] and z=U[1]. This function should return [y', z']
+        return [U[1], 4*k*U[1] + m*m*U[0]+LamB*U[0]*U[0]*U[0]]
+
     def func2(x, U):
         # Here U is a vector such that phi=U[0] and phi'=U[1]. This function should return [phi', phi'']
         return np.vstack((U[1], 4*k*U[1] + m*m*U[0]+LamB*U[0]*U[0]*U[0]))
     
-    def New_solve_bvp(Func, r0):
+    def boundaries(phi, Lam, B):
+        return(2*Lam*phi*((phi*phi)-(B*B)))
+    
+    def no_bc_bvp(Func, r0):
     
         def bc(ya, yb):
+
+            #return np.array([ya[1]-(lamH*ya[0]*(ya[0]**2-H**2)), yb[1]-(lamH*yb[0]*(yb[0]**2-V**2))])
             return np.array([ya[0]-H, yb[0]-V])
 
         xs = np.linspace(0, r0, round((2**10)*r0*r0))
         y_a = np.zeros((2, xs.size))
-        y_a[1]=1
+        y_a[1]=-0.01396517
         y_b = np.zeros((2, xs.size))
 
         #xs2 = np.linspace(0, r0, (2**12)*r0)
@@ -35,8 +43,85 @@ def Bulk_Scalar(H, V, r0, LamB=0.0, k: float = 1, m: float = 0.2, visualise: boo
 
         return([xs, y0], [xs, d0])
 
+    def bc_bvp(Func, BCs, r0):
+        
+        #creates an array of 9999 points equally spaced from 0 to r0 (the position of the visible brane)   
+        xs = np.linspace(r0, 0, 9999)
+
+
+        it=0 #counts number of interations in th shooting method
+        abserr=50 #sets the "initial" error so the the while loop can functiond
+        
+        BH0=V+0.1 #estimate for the initial boundary that the while loop will iteratively improve
+        DBH0=BCs(BH0, LamV, V) #estimate for the initial gradient
+        shoot_J=V#sets the intial interval over which the shooting method varies
+
+        # the while loop will interate, improving upon the initial conditions until it has produced the 
+        # boundary condition the the visible boundary to the error as described in the while loop condition
+        while(abserr>1e-14):
+            
+            #print(BH0)
+
+            DBH0=-BCs(BH0, LamV, V)
+            U0 = [BH0, DBH0]
+            Us0 = odeint(dU_dx, U0, xs, args=(r0, k))
+            y0 = Us0[:,0] 
+            d0 = Us0[:,1]
+            
+            #print(y0)
+
+            trunc=0
+
+            for i in range(0, y0.size):
+
+                if(((y0[i]>2*H) or (y0[i]<0)) and trunc==0):
+                    trunc=1
+                    cont=y0[i]
+                    cont2=d0[i]
+                if(trunc==1):
+                    y0[i]=cont
+                    d0[i]=cont2
+                    
+            BV1=y0[-1]#finds y value at r0
+            dsBC=BCs(BV1, LamH, H)
+            DBV1=d0[-1]
+
+
+            #for a given gradient, the following lines find the largest non imaginary solution for the boundaries
+            temp=[]
+            coeff=[-2*LamH, 0, 2*LamH*H*H, +DBV1]
+            solutions=np.roots(coeff)
+            for x in solutions:
+                if (np.iscomplex(x)==False):
+                    temp=np.append(temp, x)
+
+
+            solution=np.real(np.amax(temp))
+
+
+            err=solution-BV1
+            abserr=abs(err)
+
+
+            if(err>0):
+                    BH0=BH0+shoot_J
+            if(err<0):
+                    BH0=BH0-shoot_J
+            if(it>=500):
+                    break
+            shoot_J=shoot_J*0.5
+            it=it+1
+
+        #print("uncertainty on boundary: ", abserr)
+        return([np.flip(xs), np.flip(y0)], [np.flip(xs), np.flip(d0)])
+
+    if(LamV and LamH):
+        phi, d_phi = bc_bvp(dU_dx, boundaries, r0)
     
-    phi, d_phi = New_solve_bvp(func2, r0)  
+    else:
+        phi, d_phi = no_bc_bvp(func2, r0)
+
+
     ys=phi[1]; ds=d_phi[1]
     xs=phi[0]
     
@@ -54,56 +139,93 @@ def Bulk_Scalar(H, V, r0, LamB=0.0, k: float = 1, m: float = 0.2, visualise: boo
     
     
     if visualise:
+        fig = plt.figure(figsize=(15,8))
         plt.plot(xs, ys, label = "With Coupling")
         plt.plot(xs, Phi(AStiff, BStiff, xs), label = "Analytic Limit")
+        plt.xlabel("$y$", fontsize=13)
+        plt.ylabel("$\Phi(y)$", fontsize=13)
         plt.legend()
         plt.show()
         
     return(phi, d_phi)
 
-def Bulk_Potential(H: float, V: float, Ymin: float, Ymax: float, LamB=0.0, k: float = 1, m: float = 0.2, visualise: bool = True): 
+
+
+def Bulk_Potential(H: float, V: float, Ymin: float, Ymax: float, LamH=None, LamV=None, LamB=0.0, k: float = 1, m: float = 0.2, visualise: bool = True, value_returned=None): 
+    c=np.sqrt(4+(np.power(m,2)/np.power(k,2)))
+
+    if(Ymin==0):
+        print("Please do not set Ymin to 0, as you can't intergrate over 0 distance!!!")
+
+    E=np.sqrt(m**2)/(4*k**2)
+    #E=np.sqrt(4+(np.power(m,2)/np.power(k,2)))-2
     Potential=[]
     Y = []
     y = Ymin
     while y <= Ymax:
         
-        phi, d_phi = Bulk_Scalar(H, V, y, LamB=LamB, k=k, m=m, visualise=False)  
+        phi, d_phi = Bulk_Scalar(H, V, y, LamB=LamB, LamH=LamH, LamV=LamV, k=k, m=m, visualise=False)  
         ys=phi[1]; ds=d_phi[1]
         xs=phi[0]
 
         #the values of the field and its derivative for a given r0 are plugged into the Lagrangian
         e=np.exp(-4*k*xs)
         L=e*((ds*ds)+(m*m*ys*ys)+(LamB*ys*ys*ys)) #our lagrangean
-        Potential=np.append(Potential, simps(L, xs))
+
+        value = simps(L, xs)
+
+        if(LamV and LamH):
+            value = value + LamH*((ys[0]*ys[0]-H*H)*(ys[0]*ys[0]-H*H))+LamV*(np.exp(-4*k*y)*((ys[-1]*ys[-1]-V*V)*(ys[-1]*ys[-1]-V*V)))
+
+        Potential=np.append(Potential, value)
         Y=np.append(Y, y)
 
         y=y+0.01
 
         print(y, end="\r")
 
+    A = ((V * np.exp((c-2)*k*Y)) - H)/(np.exp(2*c*k*Y)-1)
+
+    B = H-A
+    
+    Pot=(A*A*k*(2+c)*(np.exp(2*c*k*Y)-1) + k*(c-2)*B*B*(1-np.exp(-2*c*k*Y)))
+
+    min_position_numerical=Potential.argmin()
+
+    second_deriv_num=np.gradient(np.gradient(Potential, Y), Y)
+
+    ym = Y[Potential.argmin()]
+    yc = Y[Pot.argmin()]
+
+    mass = second_deriv_num[min_position_numerical]*(np.exp(2*k*(yc+ym))/(8*k*k*k*E*E*V*V))
+
     if visualise:
-        
-        A = ((V * np.exp((c-2)*k*Y)) - H)/(np.exp(2*c*k*Y)-1)
-
-        B = H-A
-        
-        
-        Pot=(A*A*k*(2+c)*(np.exp(2*c*k*Y)-1) + k*(c-2)*B*B*(1-np.exp(-2*c*k*Y)))
-        
-
-        
-        
-        plt.plot(Y, Potential, label = "Potential")
-        plt.plot(Y, Pot)
+        fig= plt.figure(figsize=(15,8))
+        plt.plot(Y, Potential, label = "With Coupling")
+        plt.plot(Y[1:], Pot[1:], label = "Analytic Limit")
+        #print()
+        plt.ylim(0, 2*Potential[0])
         #plt.plot(xs, Phi(AStiff, BStiff, xs), label = "Analytic Limit")
+        plt.ylabel("$V_{GW}$", fontsize=13)
+        plt.xlabel("Brane Sepration $y_0$", fontsize=13)
         plt.legend()
         plt.show()
+
+
+
+        #mass1= d2Npot_dY2[NPotMinPos]*(np.exp(2*k*(Y[NPotMinPos]+Smpos))/(8*k*k*k*E*E*V*V))
         
-        print("Minima,  ", "y: ", Y[Potential.argmin()], " V: ", Potential.min())
-        print("Analytical Min, y:", Y[Pot.argmin()], "V:", Pot.min())
-
-
-    return [Y, Potential]
+        print("mr/m0: ", np.sqrt(mass))
+        print("Minima,  ", "y: ", ym, " V: ", Potential.min())
+        print("Analytical Min, y:", yc, "V:", Pot.min())
+        print("V(0): ", Potential[0], " Analytical Value: ", ((LamH*LamV)/(LamH+LamV))*(V*V - H*H)*(V*V - H*H) )
+    
+    if(value_returned == "mass"):
+        return mass
+    if(value_returned == "depth"):
+        return Potential[-1] - Potential.min()
+    else:
+        return [Y, Potential]
 
 # c=np.sqrt(4+(np.power(m,2)/np.power(k,2)))
 # E=np.sqrt(4+(np.power(m,2)/np.power(k,2)))-2
